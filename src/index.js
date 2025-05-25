6ea9dac0cb84e1bab4aacadd647728dad30c418f
Venu Gopal Avvaru <avvaruvenugopal32@gmail.com>
2025-05-25 17:06:33 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
+// var http = require('http');
+
+// //create a server object:
+// http.createServer(function (req, res) {
+//   res.write('A Monk in Cloud'); //write a response to the client
+//   res.end(); //end the response
+// }).listen(80); //the server object listens on port 80
+
+
 var http = require('http');
 
-//create a server object:
+const PORT = process.env.PORT || 8080;
+
 http.createServer(function (req, res) {
-  res.write('A Monk in Cloud'); //write a response to the client
-  res.end(); //end the response
-}).listen(80); //the server object listens on port 80
+  res.write('A Monk in Cloud');
+  res.end();
+}).listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
